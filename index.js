4aec0f0abebda18ae2795fa8347d8e855bd46f44
Dmitry <brew@bk.ru>
2021-04-10 11:18:31 +0400

    изменил homepage

---
+++ index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { ContextProvider } from 'contexts'
+import CurrentUserChecker from 'components/currentUserChecker'
+
+import routes from 'routes'
+import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
+
+import './index.css'
+
+const routesComponents = routes.map(route => {
+  return <Route path={route.path}
+    component={route.component}
+    exact={route.exact}
+    key={route.path}
+  />
+})
+
+const App = () => {
+  return (
+    <ContextProvider>
+      <CurrentUserChecker>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            {routesComponents}
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </CurrentUserChecker>
+    </ContextProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
